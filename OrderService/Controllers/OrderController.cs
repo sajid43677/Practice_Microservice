7219241163eadad8077e296b0cbf0572b94c5ba4
Shajid Kamal Joy <shajid.kamal@brainstation-23.com>
2024-10-23 19:15:56 +0600

    refactor

--- OrderService/Controllers/OrderController.cs
+++ OrderService/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
 using ProductGrpc;
 
 namespace OrderService.Controllers
@@ -24,12 +25,16 @@ namespace OrderService.Controllers
             var productResponse = await _httpClient.GetAsync("https://localhost:5000/gateway/products/product/GetAllProducts");
             var products = await productResponse.Content.ReadAsStringAsync();
 
+            using var channel = GrpcChannel.ForAddress("https://localhost:5005"); // Product service URL
+            var client = new ProductService.ProductServiceClient(channel);
+
+            var productsResponse = await client.GetAllProductsAsync(new Empty());
 
 
             var order = new
             {
                 OrderId = 123,
-                Products = products
+                Products = productsResponse.ProductReadModels
             };
 
             return Ok(order);
